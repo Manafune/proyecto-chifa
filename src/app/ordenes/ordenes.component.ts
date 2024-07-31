8817452342af75322c85ca4047da733704836bde
Manafune <laycanosmi@gmail.com>
2024-07-31 13:30:54 -0500

    refactorizacion usando form de angular

--- src/app/ordenes/ordenes.component.ts
+++ src/app/ordenes/ordenes.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { DetalleOrdenRequest, OrdenRequest } from './orden-request.model';
 import { OrderService } from '../service/ordenes.service';
 @Component({
@@ -10,39 +10,62 @@ import { OrderService } from '../service/ordenes.service';
 
 export class OrdenesComponent{
 
+  form: FormGroup;
   productos: DetalleOrdenRequest[] = [{ productoId: 0, cantidad: 0 }];
 
-  constructor(private orderService: OrderService) { }
+  private fb = inject(FormBuilder);
+  private orderService = inject(OrderService);
+  
+  constructor() {
+    this.form = this.fb.group({
+      numeroMesa: ['', [Validators.required]],
+      productos: this.fb.array([
+        this.createProducto() // Inicializa el primer producto
+      ])
+    });
+  }
 
   ngOnInit(): void {
     console.log("Hola");
     
   }
 
+  get productosArray() {
+    return this.form.get('productos') as FormArray;
+  }
+
+  createProducto(): FormGroup {
+    return this.fb.group({
+      productoId: [0, Validators.required],
+      cantidad: [0, Validators.required]
+    });
+  }
+  
   addProducto() {
-    this.productos.push({ productoId: 0, cantidad: 0 });
+    this.productosArray.push(this.createProducto());
   }
 
   removeProducto(index: number) {
-    this.productos.splice(index, 1);
+    this.productosArray.removeAt(index);
   }
 
-  async onSubmit(form: NgForm) {
-    if (form.valid) {
-      const registro: OrdenRequest = {
-        mesaId: form.value.numeroMesa,
-        detallesOrden: this.productos,
-        fechaHora: new Date().toISOString() // Ajusta la fecha según lo que necesites
+
+  async create() {
+    if (this.form.valid) {
+      const orderData: OrdenRequest = {
+        mesaId: this.form.value.numeroMesa,
+        detallesOrden: this.productosArray.value,
+        fechaHora: new Date().toISOString()
       };
       try {
-       const orderData=await this.orderService.insertar(registro)
-        form.reset()
-        this.productos = [{ productoId: 0, cantidad: 0 }];
-        console.log(orderData)
+        const result = await this.orderService.insertar(orderData);
+        this.form.reset();
+        this.productosArray.clear(); // Limpiar el array de productos
+        this.addProducto(); // Añadir al menos un producto después de resetear
+        console.log(result);
       } catch (error) {
         console.error('Error al enviar el pedido:', error);
       }
-  
     } else {
       console.error('Formulario no válido');
     }
--- src/app/ordenes/tabla-ordenes.model.ts
+++
@@ -1,6 +0,0 @@
-export interface TablaOrdenes{
-    numeroMesa: number;
-    producto: string;
-    cantidad: number;
-    fecha: string;
-}
